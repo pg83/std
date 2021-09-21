942b015c115152adc3c3afda10f30cf0ed622a01
Anton Samokhvalov <anton@samokhvalov.xyz>
2021-09-21 21:19:51 +0300

    better

--- std/alg/qsort.h
+++ std/alg/qsort.h
@@ -26,41 +26,11 @@ namespace Std::QSP {
         {
         }
 
-        inline void shellSort(I b, I e) {
-            for (auto gap : {57, 23, 10, 4}) {
-                for (auto i = b + gap; i < e; i += gap) {
-                    for (auto j = i; (j >= (b + gap)) && f(*j, *(j - gap)); j -= gap) {
-                        xchg(*j, *(j - gap));
-                    }
-                }
-            }
-
-            insertionSort(b, e);
-        }
-
         inline void insertionSort(I b, I e) {
             for (auto i = b + 1; i != e; ++i) {
                 for (auto j = i; j != b && f(*j, *(j - 1)); --j) {
                     xchg(*j, *(j - 1));
                 }
-            }
-        }
-
-        inline auto minElement(I b, I e) {
-            auto c = b;
-
-            for (; b != e; ++b) {
-                if (f(*b, *c)) {
-                    c = b;
-                }
-            }
-
-            return c;
-        }
-
-        inline void selectionSort(I b, I e) {
-            for (; b != e; ++b) {
-                xchg(*b, *minElement(b, e));
             }
         }
 
@@ -83,22 +53,7 @@ namespace Std::QSP {
         }
 
         inline auto choosePivot(I b, I e) {
-            //return b + (e - b) / 2;
-            //return median(b, b + (e - b) / 2, e - 1);
             return median(b, chooseRandom(b + 1, e - 1), e - 1);
-        }
-
-        inline auto partitionLomuto(I b, I e) {
-            auto p = e;
-            auto c = b;
-
-            for (; b != e; ++b) {
-                if (f(*b, *p)) {
-                    xchg(*b, *c++);
-                }
-            }
-
-            return c;
         }
 
         inline auto partitionHoare(I b, I e) {
@@ -135,8 +90,8 @@ namespace Std::QSP {
         }
 
         inline void qSortStep(I b, I e) {
-            if (e - b < 8) {
-                return selectionSort(b, e);
+            if (e - b < 16) {
+                return;
             }
 
             if (alreadySorted(b, e)) {
@@ -171,10 +126,7 @@ namespace Std::QSP {
             if (b != e) {
                 qSortStep(b, e);
                 qSortLoop();
-
-                //insertionSort(b, e);
-                //shellSort(b, e);
-                //selectionSort(b, e);
+                insertionSort(b, e);
             }
         }
     };
