e088cca3860b61d93ac4418b6f34e8af14bcd29e
Anton Samokhvalov <anton@samokhvalov.xyz>
2026-01-25 22:31:30 +0300

    optimize

--- std/mem/pool.cpp
+++ std/mem/pool.cpp
@@ -9,6 +9,9 @@
 
 #include <std/lib/vector.h>
 
+#include <cmath>
+#include <cstddef>
+
 using namespace Std;
 
 namespace {
@@ -17,26 +20,64 @@ namespace {
         t->~T();
     }
 
-    struct DebugPool: public Pool {
-        Vector<void*> mem;
+    struct MemoryPool {
+        inline MemoryPool() {
+            allocateNewChunk(0);
+        }
+
+        inline ~MemoryPool() noexcept {
+            for (void* chunk : chunks) {
+                freeMemory(chunk);
+            }
+        }
+
+        inline void* allocate(size_t len) {
+            constexpr size_t alignment = alignof(std::max_align_t);
+            const size_t alignedLen = (len + alignment - 1) & ~(alignment - 1);
+
+            if (currentChunk + alignedLen > currentChunkEnd) {
+                allocateNewChunk(alignedLen);
+            }
+
+            void* ptr = currentChunk;
+            currentChunk += alignedLen;
+
+            return ptr;
+        }
+
+        inline void allocateNewChunk(size_t minSize) {
+            size_t nextChunkSize = static_cast<size_t>(1024 * std::pow(2.0, chunks.length()));
+
+            if (nextChunkSize < minSize) {
+                nextChunkSize = minSize;
+            }
+
+            void* newChunk = allocateMemory(nextChunkSize);
+
+            chunks.pushBack(newChunk);
+            currentChunk = static_cast<char*>(newChunk);
+            currentChunkEnd = currentChunk + nextChunkSize;
+        }
+
+        Vector<void*> chunks;
+        char* currentChunk;
+        char* currentChunkEnd;
+    };
+
+    struct ObjectPool: public Pool {
+        MemoryPool mem;
         Vector<Dispose*> obj;
 
-        ~DebugPool() override {
+        ~ObjectPool() override {
             reverse(mutRange(obj));
 
             for (auto ptr : mutRange(obj)) {
                 destruct(ptr);
             }
-
-            for (auto ptr : mutRange(mem)) {
-                freeMemory(ptr);
-            }
         }
 
         void* allocate(size_t len) override {
-            mem.pushBack(allocateMemory(len));
-
-            return mem.back();
+            return mem.allocate(len);
         }
 
         void submit(Dispose* d) noexcept override {
@@ -49,7 +90,7 @@ Pool::~Pool() {
 }
 
 Pool::Ref Pool::fromMemory() {
-    return new DebugPool();
+    return new ObjectPool();
 }
 
 StringView Pool::intern(const StringView& s) {
