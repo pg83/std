ad6fe1d5176c58a7ebef46427dde5e6c33944803
Anton Samokhvalov <anton@samokhvalov.xyz>
2026-01-26 14:46:37 +0300

    simplify

--- std/lib/smap_ut.cpp
+++ std/lib/smap_ut.cpp
@@ -9,25 +9,25 @@ STD_TEST_SUITE(StringMap) {
     STD_TEST(test1) {
         StringMap<int> s;
 
-        s[u8"qw1"] = 1;
-        s[u8"qw2"] = 2;
+        s["qw1"] = 1;
+        s["qw2"] = 2;
 
-        STD_INSIST(s[u8"qw1"] == 1);
-        STD_INSIST(s[u8"qw2"] == 2);
-        STD_INSIST(s[u8"qw3"] == 0);
+        STD_INSIST(s["qw1"] == 1);
+        STD_INSIST(s["qw2"] == 2);
+        STD_INSIST(s["qw3"] == 0);
     }
 
     STD_TEST(test2) {
         StringMap<Vector<int>> s;
 
-        s[u8"qw1"].pushBack(1);
-        s[u8"qw1"].pushBack(2);
-        s[u8"qw2"].pushBack(3);
-        s[u8"qw2"].pushBack(4);
+        s["qw1"].pushBack(1);
+        s["qw1"].pushBack(2);
+        s["qw2"].pushBack(3);
+        s["qw2"].pushBack(4);
 
-        STD_INSIST(s[u8"qw1"][0] == 1);
-        STD_INSIST(s[u8"qw1"][1] == 2);
-        STD_INSIST(s[u8"qw2"][0] == 3);
-        STD_INSIST(s[u8"qw2"][1] == 4);
+        STD_INSIST(s["qw1"][0] == 1);
+        STD_INSIST(s["qw1"][1] == 2);
+        STD_INSIST(s["qw2"][0] == 3);
+        STD_INSIST(s["qw2"][1] == 4);
     }
 }
