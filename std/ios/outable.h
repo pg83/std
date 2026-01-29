2b85fb969954f80b96b75d725c729e1ae1f3e96f
Anton Samokhvalov <anton@samokhvalov.xyz>
2026-01-29 22:19:50 +0300

    simplify

--- std/ios/outable.h
+++ std/ios/outable.h
@@ -1,19 +1,17 @@
 #pragma once
 
-#include <std/typ/meta.h>
 #include <std/typ/intrin.h>
 
-namespace Std::IOSP {
-    // how to pass function params
-    template <typename T>
-    struct PassByValue: public Meta::Bool<sizeof(T) <= sizeof(void*) && stdIsTriviallyCopyable(T)> {
-    };
-
-    template <typename T>
-    using FuncParam = Meta::Select<PassByValue<T>::R, T, const T&>;
-}
-
 namespace Std {
-    template <typename O, typename T>
-    void output(O& out, IOSP::FuncParam<T> t);
+    template <typename T>
+    concept PassByVal = sizeof(T) <= sizeof(void*) && stdIsTriviallyCopyable(T);
+
+    template <typename T>
+    concept PassByRef = !PassByVal<T>;
+
+    template <typename O, PassByVal T>
+    void output(O& out, T t);
+
+    template <typename O, PassByRef T>
+    void output(O& out, const T& t);
 }
