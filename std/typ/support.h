81dfad1a1c654ed6831f48497b17592e54e5ea76
Anton Samokhvalov <anton@samokhvalov.xyz>
2026-01-29 22:40:56 +0300

    better

--- std/typ/support.h
+++ std/typ/support.h
@@ -1,22 +1,24 @@
 #pragma once
 
-#define rem_ref(T) __remove_reference_t(T)
 
 namespace Std {
+    template <typename T>
+    using rem_ref = __remove_reference_t(T);
+
     // move semantics
     template <typename T>
-    constexpr rem_ref(T)&& move(T&& t) noexcept {
-        return static_cast<rem_ref(T)&&>(t);
+    constexpr rem_ref<T>&& move(T&& t) noexcept {
+        return static_cast<rem_ref<T>&&>(t);
     }
 
     // perfect forwarding
     template <typename T>
-    constexpr T&& forward(rem_ref(T)& t) noexcept {
+    constexpr T&& forward(rem_ref<T>& t) noexcept {
         return static_cast<T&&>(t);
     }
 
     template <typename T>
-    constexpr T&& forward(rem_ref(T)&& t) noexcept {
+    constexpr T&& forward(rem_ref<T>&& t) noexcept {
         return static_cast<T&&>(t);
     }
 }
