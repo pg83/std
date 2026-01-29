451c1c3f5910039e3521e80f3015a146a0d6d850
Anton Samokhvalov <anton@samokhvalov.xyz>
2026-01-29 18:15:14 +0300

    better

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 
 set -xue
 
-~/ix/ix run set/dev/cc bld/make bld/sh bld/box lib/c lib/c++ lib/dwarf lib/zstd lib/rapidhash lib/cpp/trace -- make
+~/ix/ix run set/dev/cc bld/make bld/sh bld/box lib/c++ lib/rapidhash lib/cpp/trace -- make
 time ./tst/test
