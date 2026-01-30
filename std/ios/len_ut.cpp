a9e6512680721244f1ba4e1a5b9789e56d76a2c4
Anton Samokhvalov <pg@yandex-team.ru>
2026-01-30 16:43:32 +0300

    add tests

--- std/ios/len_ut.cpp
+++ std/ios/len_ut.cpp
@@ -18,4 +18,50 @@ STD_TEST_SUITE(CountingOutput) {
 
         STD_INSIST(out.collectedLength() == 50000);
     }
+
+    STD_TEST(testWriteV) {
+        CountingOutput out;
+        StringView parts[] = {
+            StringView(u8"Hello"),
+            StringView(u8" "),
+            StringView(u8"World"),
+            StringView(u8"!")
+        };
+
+        out.writeV(parts, 4);
+
+        STD_INSIST(out.collectedLength() == 12);
+    }
+
+    STD_TEST(testWriteVEmpty) {
+        CountingOutput out;
+        StringView parts[] = {
+            StringView(u8""),
+            StringView(u8"test"),
+            StringView(u8"")
+        };
+
+        out.writeV(parts, 3);
+
+        STD_INSIST(out.collectedLength() == 4);
+    }
+
+    STD_TEST(testWriteVMultipleCalls) {
+        CountingOutput out;
+        StringView parts1[] = {
+            StringView(u8"First"),
+            StringView(u8" "),
+            StringView(u8"call")
+        };
+        StringView parts2[] = {
+            StringView(u8"Second"),
+            StringView(u8" "),
+            StringView(u8"call")
+        };
+
+        out.writeV(parts1, 3);
+        out.writeV(parts2, 3);
+
+        STD_INSIST(out.collectedLength() == 21);
+    }
 }
