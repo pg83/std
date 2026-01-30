b8dbcf8fe7637c086de1e12acd8df9b294f513c4
Anton Samokhvalov <pg@yandex-team.ru>
2026-01-30 13:19:06 +0300

    better

--- std/ios/buf.cpp
+++ std/ios/buf.cpp
@@ -44,14 +44,19 @@ void OutBuf::bumpImpl(const void* ptr) noexcept {
 void OutBuf::writeImpl(const void* ptr, size_t len) {
     if (auto maxb = min<size_t>(hint(), 16 * 1024); (buf_.used() + len) < maxb) {
         buf_.append(ptr, len);
-    } else {
+    } else if (buf_.used()) {
+        Buffer buf;
+
+        buf.xchg(buf_);
+
         const StringView parts[] = {
-            StringView((const u8*)buf_.data(), buf_.used()),
+            StringView((const u8*)buf.data(), buf.used()),
             StringView((const u8*)ptr, len),
         };
 
         out_->writeV(parts, 2);
-        buf_.reset();
+    } else {
+        out_->write(ptr, len);
     }
 }
 
@@ -60,8 +65,10 @@ size_t OutBuf::hintImpl() const noexcept {
 }
 
 void OutBuf::flushImpl() {
-    out_->write(buf_.data(), buf_.used());
-    buf_.reset();
+    Buffer buf;
+
+    buf.xchg(buf_);
+    out_->write(buf.data(), buf.used());
 }
 
 void OutBuf::finishImpl() {
