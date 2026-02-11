aa5096d9e731a52ef6e1bbe987e575e0bcc2b063
Anton Samokhvalov <pg@yandex-team.ru>
2026-02-11 23:26:42 +0300

    faster

--- std/ios/in_mem.cpp
+++ std/ios/in_mem.cpp
@@ -1,4 +1,5 @@
 #include "in_mem.h"
+#include "output.h"
 
 #include <std/sys/crt.h>
 #include <std/alg/minmax.h>
@@ -12,4 +13,8 @@ size_t MemoryInput::nextImpl(const void** chunk) {
 
 void MemoryInput::commitImpl(size_t len) noexcept {
     b += len;
+}
+
+void MemoryInput::sendTo(Output& out) {
+    b += out.write(b, e - b);
 }
--- std/ios/in_mem.h
+++ std/ios/in_mem.h
@@ -9,6 +9,7 @@ namespace Std {
         const u8* b;
         const u8* e;
 
+        void sendTo(Output& out) override;
         size_t nextImpl(const void** chunk) override;
         void commitImpl(size_t len) noexcept override;
 
