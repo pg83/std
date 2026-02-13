62ee44e39b4b2fcfabffb55ed0807d62fa72508e
Anton Samokhvalov <pg@yandex-team.ru>
2026-02-13 17:52:40 +0300

    better

--- std/tst/ut_ut.cpp
+++ std/tst/ut_ut.cpp
@@ -13,14 +13,21 @@ STD_TEST_SUITE(UT) {
 
     STD_TEST(_Test2) {
         struct H: public Output {
+            ZeroCopyOutput& out;
+
+            inline H(ZeroCopyOutput& o) noexcept
+                : out(o)
+            {
+            }
+
             size_t writeImpl(const void* buf, size_t len) override {
-                sysE << len << endL << flsH;
+                out << len << endL;
 
                 return len;
             }
         };
 
-        H h;
+        H h(ctx.output());
         OutBuf ob(h, 100);
 
         for (size_t i = 0; i < 1000; ++i) {
