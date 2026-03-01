6716de3a656b5834a6c59689d716f2862f54c72e
Anton Samokhvalov <anton@samokhvalov.xyz>
2026-03-01 19:15:38 +0300

    better

--- std/sym/s_map.h
+++ std/sym/s_map.h
@@ -1,9 +1,17 @@
 #pragma once
 
 #include "h_map.h"
-#include "s_table.h"
+#include "h_table.h"
+
+#include <std/str/view.h>
 
 namespace Std {
+    struct SymbolTable: public HashTable {
+        static inline u64 hash(StringView k) noexcept {
+            return k.hash64();
+        }
+    };
+
     template <typename T>
     using SymbolMap = HashMap<T, StringView, SymbolTable>;
 }
