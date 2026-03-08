b3dd65df8bbacbdd9aaa56197876d644783d814e
Anton Samokhvalov <pg@yandex-team.ru>
2026-03-08 17:28:03 +0300

    fix

--- std/thr/pool_ut.cpp
+++ std/thr/pool_ut.cpp
@@ -298,7 +298,7 @@ STD_TEST_SUITE(WorkStealingThreadPool) {
         STD_INSIST(counter == 100);
     }
 
-    STD_TEST(_MixedWorkload) {
+    STD_TEST(MixedWorkload) {
         auto pool = ThreadPool::workStealing(4);
         int counter = 0;
         Latch latch(100);
