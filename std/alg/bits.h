5b19faeea30560d8e7bda9e8f047eff42c078c71
Anton Samokhvalov <pg@yandex-team.ru>
2026-03-09 00:41:06 +0300

    better

--- std/alg/bits.h
+++ std/alg/bits.h
@@ -3,10 +3,6 @@
 #include <std/sys/types.h>
 
 namespace stl {
-    inline u16 clp2(u16 v) noexcept {
-        return clp2((u32)v);
-    }
-
     inline u32 clp2(u32 v) noexcept {
         return v <= 1 ? 1u : 1u << (32 - __builtin_clz(v - 1));
     }
