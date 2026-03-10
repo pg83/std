c62e0347fc8c2ef0a96410d41114136e3f8799ca
Anton Samokhvalov <pg@yandex-team.ru>
2026-03-11 00:38:04 +0300

    style

--- std/thr/poller.cpp
+++ std/thr/poller.cpp
@@ -24,35 +24,45 @@ PollerIface::~PollerIface() noexcept {
 namespace {
     static u32 toEpollFlags(u32 flags) noexcept {
         u32 r = 0;
+
         if (flags & PollFlag::In) {
             r |= EPOLLIN;
         }
+
         if (flags & PollFlag::Out) {
             r |= EPOLLOUT;
         }
+
         if (flags & PollFlag::Err) {
             r |= EPOLLERR;
         }
+
         if (flags & PollFlag::Hup) {
             r |= EPOLLHUP;
         }
+
         return r;
     }
 
     static u32 fromEpollFlags(u32 events) noexcept {
         u32 r = 0;
+
         if (events & EPOLLIN) {
             r |= PollFlag::In;
         }
+
         if (events & EPOLLOUT) {
             r |= PollFlag::Out;
         }
+
         if (events & EPOLLERR) {
             r |= PollFlag::Err;
         }
+
         if (events & EPOLLHUP) {
             r |= PollFlag::Hup;
         }
+
         return r;
     }
 
@@ -70,8 +80,10 @@ namespace {
 
         void arm(int fd, u32 flags, void* data) override {
             epoll_event ev{};
+
             ev.data.ptr = data;
             ev.events = toEpollFlags(flags) | EPOLLONESHOT;
+
             if (epoll_ctl(epfd_, EPOLL_CTL_MOD, fd, &ev) < 0) {
                 STD_INSIST(errno == ENOENT);
                 STD_INSIST(epoll_ctl(epfd_, EPOLL_CTL_ADD, fd, &ev) == 0);
@@ -85,14 +97,18 @@ namespace {
         u32 wait(PollEvent* out, u32 maxEvents, u32 timeoutMs) override {
             epoll_event raw[64];
             u32 cap = maxEvents < 64 ? maxEvents : 64;
+
             int n = epoll_wait(epfd_, raw, (int)cap, (int)timeoutMs);
+
             if (n < 0) {
                 return 0;
             }
+
             for (int i = 0; i < n; i++) {
                 out[i].data = raw[i].data.ptr;
                 out[i].flags = fromEpollFlags(raw[i].events);
             }
+
             return (u32)n;
         }
     };
@@ -119,15 +135,17 @@ namespace {
 
         void arm(int fd, u32 flags, void* data) override {
             struct kevent changes[2];
+
             int n = 0;
+
             if (flags & PollFlag::In) {
-                EV_SET(&changes[n++], fd, EVFILT_READ,
-                       EV_ADD | EV_ONESHOT, 0, 0, data);
+                EV_SET(&changes[n++], fd, EVFILT_READ, EV_ADD | EV_ONESHOT, 0, 0, data);
             }
+
             if (flags & PollFlag::Out) {
-                EV_SET(&changes[n++], fd, EVFILT_WRITE,
-                       EV_ADD | EV_ONESHOT, 0, 0, data);
+                EV_SET(&changes[n++], fd, EVFILT_WRITE, EV_ADD | EV_ONESHOT, 0, 0, data);
             }
+
             if (n > 0) {
                 STD_INSIST(kevent(kqfd_, changes, n, nullptr, 0, nullptr) == 0);
             }
@@ -135,38 +153,53 @@ namespace {
 
         void disarm(int fd) override {
             struct kevent changes[2];
+
             EV_SET(&changes[0], fd, EVFILT_READ, EV_DELETE, 0, 0, nullptr);
             EV_SET(&changes[1], fd, EVFILT_WRITE, EV_DELETE, 0, 0, nullptr);
+
             kevent(kqfd_, changes, 2, nullptr, 0, nullptr);
         }
 
         u32 wait(PollEvent* out, u32 maxEvents, u32 timeoutMs) override {
             struct kevent raw[64];
+
             u32 cap = maxEvents < 64 ? maxEvents : 64;
+
             struct timespec ts;
+
             ts.tv_sec = timeoutMs / 1000;
             ts.tv_nsec = (timeoutMs % 1000) * 1000000L;
+
             int n = kevent(kqfd_, nullptr, 0, raw, (int)cap, &ts);
+
             if (n < 0) {
                 return 0;
             }
+
             for (int i = 0; i < n; i++) {
                 out[i].data = raw[i].udata;
+
                 u32 fl = 0;
+
                 if (raw[i].filter == EVFILT_READ) {
                     fl |= PollFlag::In;
                 }
+
                 if (raw[i].filter == EVFILT_WRITE) {
                     fl |= PollFlag::Out;
                 }
+
                 if (raw[i].flags & EV_EOF) {
                     fl |= PollFlag::Hup;
                 }
+
                 if (raw[i].flags & EV_ERROR) {
                     fl |= PollFlag::Err;
                 }
+
                 out[i].flags = fl;
             }
+
             return (u32)n;
         }
     };
