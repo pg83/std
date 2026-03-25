4db5ab0121f5cc916cbb342fc74e2ceccca02666
Anton Samokhvalov <pg@yandex-team.ru>
2026-03-26 02:12:10 +0300

    better

--- std/rng/mix.cpp
+++ std/rng/mix.cpp
@@ -3,20 +3,14 @@
 
 using namespace stl;
 
-namespace {
-    u64 mixPtr(const void* p) noexcept {
-        return splitMix64((uintptr_t)p);
-    }
-}
-
 u64 stl::mix(const void* a) noexcept {
-    return mixPtr(a);
+    return splitMix64((uintptr_t)a);
 }
 
 u64 stl::mix(const void* a, const void* b) noexcept {
-    return splitMix64((uintptr_t)a ^ mixPtr(b));
+    return splitMix64((uintptr_t)a ^ mix(b));
 }
 
 u64 stl::mix(const void* a, const void* b, const void* c) noexcept {
-    return splitMix64((uintptr_t)a ^ splitMix64((uintptr_t)b ^ mixPtr(c)));
+    return splitMix64((uintptr_t)a ^ mix(b, c));
 }
