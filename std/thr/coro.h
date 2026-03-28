83c50e43311f5de6598672bf63b4241dc080e206
Anton Samokhvalov <pg@yandex-team.ru>
2026-03-29 02:30:14 +0300

    style

--- std/thr/coro.h
+++ std/thr/coro.h
@@ -52,8 +52,8 @@ namespace stl {
         virtual ssize_t pwrite(int fd, const void* buf, size_t len, off_t offset) = 0;
 
         virtual EventIface* createEvent() = 0;
-        virtual CondVarIface* createCondVar() = 0;
         virtual ThreadIface* createThread() = 0;
+        virtual CondVarIface* createCondVar() = 0;
         virtual SemaphoreIface* createSemaphore(size_t initial) = 0;
 
         void sleep();
