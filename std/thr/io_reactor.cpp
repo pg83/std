5429d78948d9bb4168174509d0ea108acb1f8ec7
Anton Samokhvalov <pg@yandex-team.ru>
2026-04-10 04:44:00 +0300

    style

--- std/thr/io_reactor.cpp
+++ std/thr/io_reactor.cpp
@@ -12,11 +12,9 @@ void IoReactor::poll(PollGroup* g, VisitorFace&& visitor, u64 deadlineUs) {
 }
 
 IoReactor* IoReactor::create(ObjPool* pool, CoroExecutor* exec, size_t threads) {
-    auto io = createIoUringReactor(pool, threads);
-
-    if (!io) {
-        io = createPollIoReactor(pool, exec, threads);
+    if (auto io = createIoUringReactor(pool, threads); io) {
+        return io;
+    } else {
+        return createPollIoReactor(pool, exec, threads);
     }
-
-    return io;
 }
